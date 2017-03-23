6478664042a8ac4dd683d6d8768f4ec3e6f379b3
Jaap Boender <jaapb@kerguelen.org>
2017-03-23 13:35:53 +0000

    Updated to be more in line with algebraic branch

---
+++ complex_stuff.v
@@ -0,0 +1,92 @@
+From mathcomp Require Import all_ssreflect all_field all_real_closed all_algebra.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Parameter R: rcfType.
+
+Local Open Scope ring_scope.
+
+Definition conjugate m n (mx: 'M[R [i]]_(m, n)) :=
+  map_mx conjc mx.
+
+Definition unitarymx (n: nat) (mx: 'M[R [i]]_n) :=
+  (conjugate mx)^T *m mx = 1%:M.
+
+Lemma conj_add: forall (a b: R [i]),
+  ((a^* + b^*) = (a + b)^*)%C.
+Proof.
+  intros a b. destruct a as [ar ai]. destruct b as [br bi]. simpc. rewrite -GRing.opprD //. 
+Qed.
+
+Lemma conj_mul: forall (a b: R [i]),
+  ((a^* * b^*) = (a * b)^*)%C.
+Proof.
+  intros a b. destruct a as [ar ai]. destruct b as [br bi]. simpc. rewrite -GRing.opprD. reflexivity.
+Qed.
+
+Lemma csum: forall I (F: I -> R [i]) (r: seq I) P,
+  ((\sum_(i <- r | P) F i)^*%C) = \sum_(i <- r | P) (fun x => ((F x)^*)%C) i.
+Proof.
+  move => I F r P. induction r.
+    rewrite !big_nil. rewrite conjc0 //.
+    rewrite !big_cons. destruct P.
+      rewrite -conj_add. rewrite IHr. reflexivity.
+      apply IHr.
+Qed.
+
+Lemma cprod: forall I (F: I -> R[i]) (r: seq I) P,
+  ((\prod_(i <- r | P) F i)^*%C) = \prod_(i <- r | P) (fun x => ((F x)^*)%C) i.
+Proof.
+  move => I F r P. induction r.
+    rewrite !big_nil. apply conjc1.
+    rewrite !big_cons. destruct P.
+      rewrite -conj_mul. rewrite IHr. reflexivity.
+      apply IHr.
+Qed.
+
+Lemma conjc_opp: forall (a: R[i]), (-(a^*) = (-a)^*)%C.
+Proof.
+  intros a. destruct a as [ar ai]. simpc. simpl. rewrite GRing.opprK. reflexivity. 
+Qed.
+
+Lemma conjcN1: ((-1: R[i])^* )%C = (-1)%C.
+Proof.
+  rewrite -conjc_opp. rewrite conjc1. reflexivity.
+Qed.
+
+Lemma nbn: forall (x y: R [i]), x = y -> -x = -y.
+Proof.
+  intros. rewrite H. reflexivity.
+Qed.
+
+Lemma conjugate_det n (mx: 'M[R [i]]_n):
+  (\det (conjugate mx)%C) = ((\det mx)^*%C)%R.
+Proof.
+  unfold determinant. rewrite csum. apply eq_big_seq. intros x _. rewrite -conj_mul.
+  unfold GRing.exp. destruct (perm.odd_perm x).
+    rewrite conjcN1. rewrite !GRing.mulN1r. 
+    rewrite cprod. unfold conjugate. unfold map_mx. apply nbn. apply eq_bigr. intros i _. apply mxE.
+    rewrite conjc1. rewrite !GRing.mul1r.
+    rewrite cprod. unfold conjugate. unfold map_mx. apply eq_bigr. intros i _. apply mxE.
+Qed.
+
+Definition abs_sqc (x: R [i]): R := 
+  let: (a +i* b)%C := x in a ^+ 2 + b ^+ 2.
+
+Lemma transpose_abs: forall (x: R[i]),
+  x * (x^*)%C = ((abs_sqc x)%:C)%C.
+Proof.
+  destruct x as [a b]. unfold abs_sqc. simpc. rewrite (GRing.mulrC b a). rewrite GRing.addNr. 
+  reflexivity.
+Qed.
+
+Lemma unitary_det n (mx: 'M[R [i]]_n):
+  unitarymx mx -> abs_sqc (\det mx) = 1.
+Proof.
+  move => unitary_mx.
+  assert (((abs_sqc (\det mx))%:C) = 1%:C)%C.
+    rewrite -transpose_abs. rewrite -conjugate_det. rewrite GRing.mulrC. rewrite -det_tr.
+  rewrite -det_mulmx. rewrite unitary_mx. rewrite det1. auto.
+  inversion H. reflexivity.
+Qed.
--- quantum.v
+++ quantum.v
@@ -1,31 +1,33 @@
-Require Import ssreflect.
-Require Import complex.
-Require Import ssrnum.
-Require Import matrix.
-Require Import fintype.
-Require Import ssrnat.
-(*Require Import bigop.*)
-Require Import ssralg.
-(*Require Import mxalgebra.*)
-Require Import eqtype.
+From mathcomp Require Import all_ssreflect all_real_closed all_algebra.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
 
-Open Scope complex_scope.
+Local Open Scope ring_scope.
 
-Definition abs_sqc (R: rcfType) (x: R [i]) := 
-  let: a +i* b := x in (a ^+ 2 + b ^+ 2)%R.
+Require Import complex_stuff.
 
-Record qubit_mixin_of (R: rcfType) (n: nat): Type := QubitMixin {
-  vector: 'cV[R [i]]_(2 ^ n);
-  vector_is_unit: is_true ((\sum_(i < 2^n) (abs_sqc _ (vector i (Ordinal (ltnSn 0)))))%R == 1%R)
+Record qubit_mixin_of (n: nat) := QubitMixin {
+  vector: 'cV[complex_random_stuff.R [i]]_(2 ^ n);
+  vector_is_unit: is_true (\sum_(i < 2^n) (abs_sqc (vector i (Ordinal (ltnSn 0)))) == 1)
 }.
 
-Record gate_mixin_of (R: rcfType) (n: nat): Type := GateMixin {
-  gate: 'M[R [i]]_(2 ^ n);
-  thingie: is_true (unitmx gate)
+Record gate_mixin_of (n: nat): Type := GateMixin {
+  gate: 'M[complex_random_stuff.R [i]]_(2 ^ n);
+  gate_is_unitary: unitarymx gate
 }.
 
+Lemma blerp: forall n q g, is_true
+   ((\sum_(i < 2 ^ n)
+        abs_sqc
+          (((gate g) *m vector q) i (Ordinal (n:=1) (m:=0) (ltnSn 0))))%R ==
+    1%R).
+Proof.
+  intros n q g; destruct q as [q Hq]; destruct g as [g Hg]; simpl.
 
-
+Definition apply (R: rcfType) (n: nat) (q: qubit_mixin_of R n) (g: gate_mixin_of R n): qubit_mixin_of R n :=
+  QubitMixin 
+    (blerp q g).
 
 
 
