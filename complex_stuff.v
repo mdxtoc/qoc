d2eac3b7c9160fb7c8c412702199add65775f19e
Jaap Boender <jaapb@kerguelen.org>
2017-04-04 15:11:29 +0100

    Committed latest files

--- complex_stuff.v
+++ complex_stuff.v
@@ -14,35 +14,18 @@ Definition conjugate m n (mx: 'M[R [i]]_(m, n)) :=
 Definition unitarymx (n: nat) (mx: 'M[R [i]]_n) :=
   (conjugate mx)^T *m mx = 1%:M.
 
-Lemma rc_add: forall (a b: R),
-  (a%:C + b%:C = (a + b)%:C)%C.
-Proof.
-  intros a b. unfold real_complex. simpc. reflexivity.
-Qed.
-
 Lemma conj_add: forall (a b: R [i]),
   ((a^* + b^*) = (a + b)^*)%C.
 Proof.
-  intros a b. destruct a as [ar ai]. destruct b as [br bi]. simpc. rewrite -GRing.opprD //. 
+  intros a b; destruct a as [ar ai]; destruct b as [br bi]; simpc; rewrite -GRing.opprD //.
 Qed.
 
 Lemma conj_mul: forall (a b: R [i]),
   ((a^* * b^*) = (a * b)^*)%C.
 Proof.
-  intros a b. destruct a as [ar ai]. destruct b as [br bi]. simpc. rewrite -GRing.opprD. reflexivity.
+  intros a b; destruct a as [ar ai]; destruct b as [br bi]; simpc; rewrite -GRing.opprD; reflexivity.
 Qed.
 
-Lemma rcsum: forall I (F: I -> R) (r: seq I) P,
-  (\sum_(i <- r | P) (F i)%:C)%C = ((\sum_(i <- r | P) F i)%:C)%C.
-Proof.
-  move => I F r P. induction r.
-    rewrite !big_nil //.
-    rewrite !big_cons; destruct P;
-    [ rewrite IHr; rewrite rc_add //
-    | apply IHr
-    ].
-Qed.
-   
 Lemma csum: forall I (F: I -> R [i]) (r: seq I) P,
   ((\sum_(i <- r | P) F i)^*%C) = \sum_(i <- r | P) (fun x => ((F x)^*)%C) i.
 Proof.
@@ -84,30 +67,30 @@ Proof.
   unfold determinant. rewrite csum. apply eq_big_seq. intros x _. rewrite -conj_mul.
   unfold GRing.exp. destruct (perm.odd_perm x).
     rewrite conjcN1. rewrite !GRing.mulN1r. 
-    rewrite cprod. unfold conjugate. unfold map_mx. apply nbn. apply eq_bigr. intros i _. apply mxE.
+    rewrite cprod. apply nbn. apply eq_bigr. intros i _. apply mxE.
     rewrite conjc1. rewrite !GRing.mul1r.
     rewrite cprod. unfold conjugate. unfold map_mx. apply eq_bigr. intros i _. apply mxE.
 Qed.
 
-Definition abs_sqc (x: R [i]): R := 
+(*Definition abs_sqc (x: R [i]): R := 
   let: (a +i* b)%C := x in a ^+ 2 + b ^+ 2.
 
 Lemma transpose_abs: forall (x: R[i]),
-  x * (x^*)%C = ((abs_sqc x)%:C)%C.
+  x * (x^* )%C = ((abs_sqc x)%:C)%C.
 Proof.
   destruct x as [a b]. unfold abs_sqc. simpc. rewrite (GRing.mulrC b a). rewrite GRing.addNr. 
   reflexivity.
-Qed.
+Qed.*)
 
-Lemma unitary_det n (mx: 'M[R [i]]_n):
-  unitarymx mx -> abs_sqc (\det mx) = 1.
+(*Lemma unitary_det n (mx: 'M[R [i]]_n):
+  unitarymx mx -> \det mx = 1.
 Proof.
-  move => unitary_mx.
-  assert (((abs_sqc (\det mx))%:C) = 1%:C)%C.
-    rewrite -transpose_abs. rewrite -conjugate_det. rewrite GRing.mulrC. rewrite -det_tr.
+  move => unitary_mx. rewrite -[1]mulr1. rewrite -{2}conjc1. rewrite -sqr_normc. rewrite -det_mulmx.
+  (*assert (((abs_sqc (\det mx))%:C) = 1%:C)%C.
+    rewrite -transpose_abs. rewrite -conjugate_det. rewrite GRing.mulrC. rewrite -det_tr.*)
   rewrite -det_mulmx. rewrite unitary_mx. rewrite det1. auto.
   inversion H. reflexivity.
-Qed.
+Qed.*)
 
 Lemma conjugate_mulmx: forall m n p (mx1: 'M_(m, n)) (mx2: 'M_(n, p)),
   conjugate (mx1 *m mx2) = conjugate mx1 *m conjugate mx2.
@@ -125,10 +108,8 @@ Qed.
 
 Lemma bloitbeard:
   forall n (v: 'cV[R [i]]_n),
-    ((conjugate v)^T *m v) 0 0 = ((\sum_(i < n) abs_sqc (v i 0))%:C)%C.
+    ((conjugate v)^T *m v) 0 0 = \sum_(i < n) `|v i 0|^+2.
 Proof.
-  intros n v. rewrite !mxE. rewrite -rcsum. apply eq_bigr; intros i _.
-  rewrite !mxE. rewrite mulrC. apply transpose_abs.
+  intros n v. rewrite !mxE. apply eq_bigr; intros i _.
+  rewrite !mxE. rewrite mulrC. symmetry; apply sqr_normc.
 Qed.
-     
-   
--- quantum.v
+++ quantum.v
@@ -10,7 +10,7 @@ Require Import complex_stuff.
 
 Record qubit_mixin_of (n: nat) := QubitMixin {
   vector: 'cV[complex_stuff.R [i]]_(2 ^ n);
-  vector_is_unit: ((\sum_(i < 2^n) (abs_sqc (vector i 0)))%:C = 1)%C
+  vector_is_unit: \sum_(i < 2^n) `|vector i 0|^+2 = 1
 }.
 
 Record gate_mixin_of (n: nat): Type := GateMixin {
@@ -19,12 +19,12 @@ Record gate_mixin_of (n: nat): Type := GateMixin {
 }.
 
 Lemma blerp: forall n q g,
-  ((\sum_(i < 2 ^ n) (abs_sqc (((gate g) *m vector q) i 0)))%:C = 1)%C.
+  \sum_(i < 2 ^ n) `|(gate g *m vector q) i 0|^+2 = 1.
 Proof.
   intros n q g; destruct q as [q Hq]; destruct g as [g Hg]; simpl.
   rewrite -bloitbeard. rewrite -gniarf;
-  [ rewrite !mxE; rewrite -Hq; rewrite -rcsum; apply eq_bigr; intros i _;
-    rewrite mulrC; rewrite !mxE; apply transpose_abs
+  [ rewrite !mxE; rewrite -Hq; apply eq_bigr; intros i _;
+    rewrite mulrC; rewrite !mxE; symmetry; apply sqr_normc
   | apply Hg
   ]. 
 Qed.
@@ -42,7 +42,32 @@ Definition I_matrix: 'M[R [i]]_2 :=
   'M{[:: [:: 1; 0];
          [:: 0; 1]]}.
 
-(* Lemma I_unitary: unitarymx I_matrix. *)
+(* this can probably be done by a tactic, or computationally *)
+Lemma I_unitary: unitarymx I_matrix.
+Proof.
+  apply/matrixP. intros x y; rewrite !mxE. rewrite !big_ord_recl. rewrite big_ord0; rewrite !mxE.
+  destruct x as [m H]; destruct m as [ | m]; destruct y as [m' H']; destruct m' as [ | m'];
+  [ 
+  | destruct m' as [ | m'];
+    [ rewrite mulr0; rewrite add0r; rewrite addr0
+    | absurd (m'.+2 < 2)%N; auto; apply H'
+    ]
+  | destruct m as [ | m ];
+    [
+    | absurd (m.+2 < 2)%N; auto; apply H
+    ]
+  | destruct m as [ | m ];
+    [ destruct m' as [ | m' ];
+      [ 
+      | absurd (m'.+2 < 2)%N; auto; apply H'
+      ]
+    | absurd (m.+2 < 2)%N; auto; apply H
+    ]
+  ]; simpl; try repeat (rewrite mulr0 || rewrite mul0r || rewrite addr0 || rewrite add0r || rewrite mul1r || rewrite mulr1);
+  rewrite oppr0 //.
+Qed.
+
+Definition I_gate := (@GateMixin 1 I_matrix I_unitary).
 
 Definition X_matrix: 'M[R [i]]_2 :=
   'M{[:: [:: 0; 1];
@@ -59,3 +84,11 @@ Definition Z_matrix: 'M[R [i]]_2 :=
 Definition hadamard_matrix: 'M[R [i]]_2 :=
   ('M{[:: [:: (1/Num.sqrt (2%:R))%:C; (1/Num.sqrt (2%:R))%:C];
           [:: (1/Num.sqrt (2%:R))%:C; -(1/Num.sqrt (2%:R))%:C]]})%C.
+
+Definition select n (i: 'I_(2^n)) (bit: 'I_n) :=
+  (*i mod (2^(bit + 1)) >= 2^bit.*)
+  true.
+
+Definition measure n (bit: 'I_n) (qubit: qubit_mixin_of n) :=
+  \col_(i < 2^n) if select i bit then ((vector qubit) i 0) / \sum_(i < 2^n | select i bit) ((vector qubit) i 0)^+2 else 0.
+
