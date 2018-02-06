e53b0eb0a5b8b9936cce1ab121de26b6b90a8f89
Jaap Boender <jaapb@kerguelen.org>
2018-02-06 17:49:56 +0000

    Extended sum_cast

--- other_stuff.v
+++ other_stuff.v
@@ -32,7 +32,7 @@ Proof.
     intros; rewrite !big_ord0 //.
     intros; inversion Heq. 
     intros; inversion Heq.
-    intros. clear H n0 m. rewrite !big_ord_recl.   
+    intros. clear H n0 m. rewrite !big_ord_recl.
     inversion Heq. rewrite (H0 n _ (fun x => F2 (fintype.lift ord0 x)) H2).
       rewrite H1.
    replace (F2 (cast_ord Heq ord0)) with (F2 ord0).
@@ -43,4 +43,35 @@ Proof.
      rewrite (eq_irrelevance (@cast_ord_proof (S n1) (S n) (@Ordinal (S n1) _ (@lift_subproof (S n1) 0 x)) Heq)
        (@lift_subproof (S n) 0 (@Ordinal n _ (@cast_ord_proof n1 n x H2)))
      ). reflexivity.
+Qed.
+
+Lemma sum_cast2: forall (R: zmodType) m n (Heq: m = n) (P1: pred 'I_m) (P2: pred 'I_n)
+  (F1: 'I_m -> R) (F2: 'I_n -> R),
+  (forall x, P1 x = P2 (cast_ord Heq x)) ->
+  (forall x, F1 x = F2 (cast_ord Heq x)) ->
+  \sum_(i < m | P1 i) F1 i =
+  \sum_(i < n | P2 i) F2 i.
+Proof.
+  intros.
+    replace (\sum_(i < m | P1 i) F1 i) with (\sum_(i < m) (if P1 i then F1 i else 0)).
+    replace (\sum_(i < n | P2 i) F2 i) with (\sum_(i < n) (if P2 i then F2 i else 0)).
+  generalize P1 P2 F1 F2 Heq H H0. clear P1 P2 F1 F2 H H0 Heq. generalize m n. clear m n.
+  double induction m n.
+    intros; rewrite !big_ord0 //.
+    intros; inversion Heq.
+    intros; inversion Heq.
+    intros. clear H n0 m.
+      rewrite !big_ord_recl. inversion Heq.
+      rewrite (H0 n _ (fun x => P2 (fintype.lift ord0 x)) _ (fun x => F2 (fintype.lift ord0 x))).
+      rewrite H1. rewrite H2. replace (F2 (cast_ord Heq ord0)) with (F2 ord0).
+      replace (P2 (cast_ord Heq ord0)) with (P2 ord0). reflexivity.
+      compute. rewrite (eq_irrelevance (ltn0Sn n) (cast_ord_proof (Ordinal (ltn0Sn n1)) Heq)). reflexivity.
+      compute. rewrite (eq_irrelevance (ltn0Sn n) (cast_ord_proof (Ordinal (ltn0Sn n1)) Heq)). reflexivity.
+    intros; rewrite H1. compute. rewrite (eq_irrelevance (@cast_ord_proof (S n1) (S n) (@Ordinal (S n1) _ (@lift_subproof (S n1) 0 x)) Heq)
+       (@lift_subproof (S n) 0 (@Ordinal n _ (@cast_ord_proof n1 n x H3)))
+     ). reflexivity.
+    intros; rewrite H2. compute. rewrite (eq_irrelevance (@cast_ord_proof (S n1) (S n) (@Ordinal (S n1) _ (@lift_subproof (S n1) 0 x)) Heq)
+       (@lift_subproof (S n) 0 (@Ordinal n _ (@cast_ord_proof n1 n x H3)))
+     ). reflexivity.
+   rewrite -big_mkcond. reflexivity. rewrite -big_mkcond. reflexivity.
 Qed.
