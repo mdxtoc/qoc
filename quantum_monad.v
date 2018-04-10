dd354238dc7e7ff6b2d70d8c302d2c8aa09e784f
Jaap Boender <jaapb@kerguelen.org>
2018-04-10 14:29:15 +0100

    Added QIO type and proved that it can engender a monad

--- quantum_monad.v
+++ quantum_monad.v
@@ -19,29 +19,25 @@ Definition gjoin {I O O' A B m} `{ GMonad m } (e1 : m O' O (A -> m I O' B)) (x :
   f <-- e1 ;; f x.
 
 Class Monad (m: Type -> Type): Type :=
-{ return_: forall A, A -> m A;
-  bind: forall A, m A -> forall B, (A -> m B) -> m B;
-  right_unit: forall A (a: m A), a = bind a (@return_ A);
+{ return_: forall {A}, A -> m A;
+  bind_: forall {A}, m A -> forall {B}, (A -> m B) -> m B;
+  right_unit: forall A (a: m A), a = bind_ a (@return_ A);
   left_unit: forall A (a: A) B (f: A -> m B),
-             f a = bind (return_ a) f;
+             f a = bind_ (return_ a) f;
   associativity: forall A (ma: m A) B f C (g: B -> m C),
-                 bind ma (fun x => bind (f x) g) = bind (bind ma f) g
+                 bind_ ma (fun x => bind_ (f x) g) = bind_ (bind_ ma f) g
 }.
 
 Require Import Coq.Program.Tactics.
-Program Instance monad_is_gmonad {m} `{Monad m}: GMonad (fun i o => m) :=
+Instance monad_is_gmonad {m} `{Monad m}: GMonad (fun i o => m) :=
 {
   gunit i a x := return_ x;
-  gbind i o o' a b m f := bind m f
+  gbind i o o' a b m f := bind_ m f
 }.
-Obligation 1.
+Proof.
   symmetry; apply left_unit.
-Qed.
-Obligation 2.
   symmetry; apply right_unit.
-Qed.
-Obligation 3.
-  apply associativity.
+  intros; apply associativity.
 Qed.
 
 From mathcomp Require Import all_ssreflect all_algebra all_field all_real_closed.
@@ -49,4 +45,44 @@ Local Open Scope ring_scope.
 Import Num.Theory GRing.Theory.
 
 Require Import quantum.
-Program Instance blerp: Monad (list (R[i] * qubit_mixin_of n)).
+
+(* The Qbit type is represented here by a (qubit_vector_of 1) *)
+
+Definition Qbit := qubit_vector_of 1.
+Definition U := gate_of 1.
+Inductive QIO (a: Type): Type :=
+  QReturn: a -> QIO a
+| MkQbit: bool -> (Qbit -> QIO a) -> QIO a
+| ApplyU: U -> QIO a -> QIO a
+| Meas: Qbit -> (bool -> QIO a) -> QIO a.
+
+Fixpoint qio_bind {A: Type} (z: QIO A) {B: Type} (f: A -> QIO B) :=
+  match z with
+  | QReturn a => f a
+  | MkQbit b g => MkQbit b (fun x => qio_bind (g x) f)
+  | ApplyU u q => ApplyU u (qio_bind q f)
+  | Meas x g => Meas x (fun b => qio_bind (g b) f)
+  end.
+
+Require Import Coq.Logic.FunctionalExtensionality.
+Instance qio_monad: Monad QIO := 
+{
+  return_ := QReturn;
+  bind_ _ z _ f := qio_bind z f
+}.
+Proof.
+  intros. induction a; simpl;
+  [ 
+  | f_equal; extensionality x; rewrite {1}H
+  | rewrite {1}IHa
+  | f_equal; extensionality x; rewrite {1}H
+  ];
+  try reflexivity.
+  intros; simpl; reflexivity.
+  intros; induction ma; simpl;
+  [ reflexivity 
+  | f_equal; extensionality x; apply H
+  | f_equal; apply IHma
+  | f_equal; extensionality x; apply H
+  ].
+Qed.
